08b7bbfb21f5e1155179de8ef526801f1ccc9c7e
79155637 <david.velandia.laverde@gmail.com>
2022-11-08 02:11:50 -0500

    Generación SMS y E-mails por Twilio y Sengrid Generación de Clave desde el programa enviada al E-mail y Cifrado de la msima en la creación de un Registro de Persona

--- src/services/autenticacion.service.ts
+++ src/services/autenticacion.service.ts
@@ -1,4 +1,10 @@
+/* eslint-disable prefer-const */
+/* eslint-disable @typescript-eslint/naming-convention */
 import {injectable, /* inject, */ BindingScope} from '@loopback/core';
+/*import generador from 'password-generator';
+import cryptoJS from 'crypto-js';*/
+const generador = require('password-generator');
+const cryptoJS = require('crypto-js');
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class AutenticacionService {
@@ -7,4 +13,13 @@ export class AutenticacionService {
   /*
    * Add service methods here
    */
+
+  GenerarClave(){
+    let clave = generador(8, false);
+    return clave;
+  }
+  CifrarClave(clave:string){
+    let claveCifrada = cryptoJS.MD5(clave).toString();
+    return claveCifrada;
+  }
 }
