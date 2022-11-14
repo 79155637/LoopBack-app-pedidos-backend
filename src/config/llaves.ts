6c845e3e18d08863a6966642f9160a5a06dcd842
79155637 <david.velandia.laverde@gmail.com>
2022-11-14 09:23:12 -0500

    Integración con el FrontEnd hecho con Bootstrap

--- src/config/llaves.ts
+++ src/config/llaves.ts
@@ -1,4 +1,5 @@
 export namespace Llaves{
   export const claveJWT = "JWT@2022*";
-  export const urlServiciosNotificaciones = 'http://localhost:5000';
+  export const urlServiciosNotificaciones = 'http://127.0.0.1:5000';
+  /*'http://localhost:5000'; No me funciono, desde el FrontEnd hecho con Bootstrap, abortaba el servidor de la App de LoopBack, cuando se creaba una nueva persona, buscando en Internet el ERROR aconsejaba hacer el cambio implementado y funciono */
 }
